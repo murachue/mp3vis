29bf223499f3e983507e42ee472774256ee882ad
Murachue <murachue+github@gmail.com>
2020-08-11 21:17:15 +0900

    SFG stereoed support

--- src/ScalefacFreqGraph.tsx
+++ src/ScalefacFreqGraph.tsx
@@ -36,36 +36,74 @@ export function ScalefacFreqGraph({ data, granule, channel, which, ...props }: S
             ctx.fillRect(w * xbar, ymin, w, ymax - ymin);
         };
 
-        const drawGrid = () => {
-            if (channel === null) {
+        const sampfreq = sampling_frequencies[data.frame.header.sampling_frequency];
+        const sfblong = scalefactor_band_indices_long[sampfreq];
+        const sfbshort = scalefactor_band_indices_short[sampfreq];
+
+        const sideinfo_gr_ch = data.frame.sideinfo.channel[channel || 0].granule[granule];
+        const maindata_gr_ch = data.maindata!.granule[granule].channel[channel || 0];
+
+        const scale_step = sideinfo_gr_ch.scalefac_scale ? 1 : 0.5;
+        const global_gain = sideinfo_gr_ch.global_gain;
+
+        const onepixelStep = (zoom: number, scalefac: number, subblock_gain: number) =>
+            Math.max(1, powReal((1 / zoom) / requantizeMultiplier(scale_step, scalefac, global_gain, subblock_gain), 3 / 4)); // reverse-requantize 1/zoom
+
+        const drawLongGridTill = (till: number) => {
+            ctx.fillStyle = "#ddd";
+            for (const i of times(till)) {
+                vbar(sfblong[i], 0, ch);
+            }
+
+            if (which === "stereoed") {
                 return;
             }
 
-            const sideinfo_gr_ch = data.frame.sideinfo.channel[channel].granule[granule];
-            const maindata_gr_ch = data.maindata!.granule[granule].channel[channel];
+            const scalefac_l = maindata_gr_ch.scalefac.scalefac_l?.concat([0]); // [0] for very last non-encoded sfband.
+            const cy = ch / 2;
 
-            const sampfreq = sampling_frequencies[data.frame.header.sampling_frequency];
-            const sfblong = scalefactor_band_indices_long[sampfreq];
-            const sfbshort = scalefactor_band_indices_short[sampfreq];
+            ctx.strokeStyle = "#eee";
+            for (const i of times(till)) {
+                const scalefac = scalefac_l![i];
+                const x = sfblong[i];
+                const x2 = sfblong[i + 1];
+                const step = onepixelStep(1, scalefac, 0);
+                for (const y of range(0, 8192, step)) {
+                    const ry = Math.floor(requantizeSample(y, scale_step, scalefac, global_gain, 0));
+                    if (cy < ry) {
+                        break;
+                    }
+                    hline(x, cy - ry, x2);
+                    hline(x, cy + ry, x2);
+                }
+            }
+        };
+        const drawShortGridFrom = (from: number) => {
+            const scalefac_s = maindata_gr_ch.scalefac.scalefac_s?.concat([[0, 0, 0]]); // [[0,0,0]] for very last non-encoded sfband.;
+            const cy = ch / 2;
 
-            const scale_step = sideinfo_gr_ch.scalefac_scale ? 1 : 0.5;
-            const global_gain = sideinfo_gr_ch.global_gain;
+            const subgrid = which === "requantized";
+            for (const i of range(from, sfbshort.length - 1)) {
+                for (const wi of times(3)) {
+                    if (!subgrid && wi !== 0) {
+                        continue;
+                    }
 
-            const onepixelStep = (zoom: number, scalefac: number, subblock_gain: number) =>
-                Math.max(1, powReal((1 / zoom) / requantizeMultiplier(scale_step, scalefac, global_gain, subblock_gain), 3 / 4)); // reverse-requantize 1/zoom
+                    ctx.fillStyle = wi === 0 ? "#ddd" : "#eee";
+                    const x = sfbshort[i] * 3 + (sfbshort[i + 1] - sfbshort[i]) * wi;
+                    vbar(x, 0, ch);
 
-            const drawLongGridTill = (till: number) => {
-                const scalefac_l = maindata_gr_ch.scalefac.scalefac_l?.concat([0]); // [0] for very last non-encoded sfband.
-                const cy = ch / 2;
+                    if (!subgrid) {
+                        continue;
+                    }
 
-                ctx.strokeStyle = "#eee";
-                for (const i of times(till)) {
-                    const scalefac = scalefac_l![i];
-                    const x = sfblong[i];
-                    const x2 = sfblong[i + 1];
-                    const step = onepixelStep(1, scalefac, 0);
+                    ctx.strokeStyle = "#eee";
+                    const scalefac = scalefac_s![i][wi];
+                    const subblock_gain = sideinfo_gr_ch.subblock_gain![wi];
+                    const x2 = sfbshort[i] * 3 + (sfbshort[i + 1] - sfbshort[i]) * (wi + 1);
+                    const step = onepixelStep(1, scalefac, subblock_gain);
                     for (const y of range(0, 8192, step)) {
-                        const ry = Math.floor(requantizeSample(y, scale_step, scalefac, global_gain, 0));
+                        const ry = Math.floor(requantizeSample(y, scale_step, scalefac, global_gain, subblock_gain));
                         if (cy < ry) {
                             break;
                         }
@@ -73,66 +111,44 @@ export function ScalefacFreqGraph({ data, granule, channel, which, ...props }: S
                         hline(x, cy + ry, x2);
                     }
                 }
-
-                ctx.fillStyle = "#ddd";
-                for (const i of times(till)) {
-                    vbar(sfblong[i], 0, ch);
-                }
-            };
-            const drawShortGridFrom = (from: number) => {
-                const scalefac_s = maindata_gr_ch.scalefac.scalefac_s?.concat([[0, 0, 0]]); // [[0,0,0]] for very last non-encoded sfband.;
-                const cy = ch / 2;
-
-                const subgrid = which === "requantized";
-                for (const i of range(from, sfbshort.length - 1)) {
-                    for (const wi of times(3)) {
-                        if (!subgrid && wi !== 0) {
-                            continue;
-                        }
-
-                        ctx.fillStyle = wi === 0 ? "#ddd" : "#eee";
-                        const x = sfbshort[i] * 3 + (sfbshort[i + 1] - sfbshort[i]) * wi;
-                        vbar(x, 0, ch);
-
-                        if (!subgrid) {
-                            continue;
-                        }
-
-                        ctx.strokeStyle = "#eee";
-                        const scalefac = scalefac_s![i][wi];
-                        const subblock_gain = sideinfo_gr_ch.subblock_gain![wi];
-                        const x2 = sfbshort[i] * 3 + (sfbshort[i + 1] - sfbshort[i]) * (wi + 1);
-                        const step = onepixelStep(1, scalefac, subblock_gain);
-                        for (const y of range(0, 8192, step)) {
-                            const ry = Math.floor(requantizeSample(y, scale_step, scalefac, global_gain, subblock_gain));
-                            if (cy < ry) {
-                                break;
-                            }
-                            hline(x, cy - ry, x2);
-                            hline(x, cy + ry, x2);
-                        }
-                    }
-                }
-            };
-
-            drawLongGridTill(sideinfo_gr_ch.block_type !== 2 ? sfblong.length - 1 : sideinfo_gr_ch.switch_point ? 9 : 0);
-            drawShortGridFrom(sideinfo_gr_ch.block_type !== 2 ? sfbshort.length - 1 : sideinfo_gr_ch.switch_point ? 2 : 0);
-
-            if (sideinfo_gr_ch.block_type === 2 && sideinfo_gr_ch.switch_point) {
-                ctx.fillStyle = "#fdd";
-                vbar(36, 0, ch);
             }
         };
 
-        drawGrid();
+        const blockStyle = (ch: number) => {
+            const sideinfo_gr_ch = data.frame.sideinfo.channel[ch].granule[granule];
+            return sideinfo_gr_ch.block_type !== 2 ? "long" : sideinfo_gr_ch.switch_point ? "mixed" : "short";
+        };
+        if (which !== "stereoed" || data.frame.sideinfo.channel.length < 2 || blockStyle(0) === blockStyle(1)) {
+            const style = blockStyle(channel || 0);
+            drawLongGridTill({ long: sfblong.length - 1, mixed: 9, short: 0 }[style]);
+            drawShortGridFrom({ long: sfbshort.length - 1, mixed: 2, short: 0 }[style]);
+        };
+
+        if (sideinfo_gr_ch.block_type === 2 && sideinfo_gr_ch.switch_point) {
+            ctx.fillStyle = "#fdd";
+            vbar(36, 0, ch);
+        }
 
         ctx.strokeStyle = "#ddd";
         hline(0, ch / 2, cw);
 
-        ctx.fillStyle = "red";
-        const samples = data.internal![which].granule[granule].channel[channel || 0].samples;
-        for (const x of times(576)) {
-            vbar(x, ch / 2, (samples[x] + 1) * ch / 2);
+        const chans = data.internal![which].granule[granule].channel;
+        if (channel !== null) {
+            ctx.fillStyle = "red";
+            const samples = chans[channel].samples;
+            for (const x of times(576)) {
+                vbar(x, ch / 2, (samples[x] + 1) * ch / 2);
+            }
+        } else {
+            const colors = chans.length < 2 ? ["red"] : ["#0c0", "#66f"];
+            ctx.globalAlpha = 0.5;
+            for (const ch_i in chans) {
+                ctx.fillStyle = colors[ch_i];
+                const samples = chans[ch_i].samples;
+                for (const x of times(576)) {
+                    vbar(x, ch / 2, (samples[x] + 1) * ch / 2);
+                }
+            };
         }
     };
 
