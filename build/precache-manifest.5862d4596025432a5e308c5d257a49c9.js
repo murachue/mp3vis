95aa0246b099bf090af2fc7eddd94a40fba6a423
murachue <murachue@users.noreply.github.com>
2020-09-07 15:05:10 +0000

    built for gh-pages

---
+++ build/precache-manifest.5862d4596025432a5e308c5d257a49c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7334f2906b48a5e9767fbba46b10b78",
+    "url": "./index.html"
+  },
+  {
+    "revision": "338c51124395d60b63ec",
+    "url": "./static/css/main.c74dc28b.chunk.css"
+  },
+  {
+    "revision": "38489138691b865bef15",
+    "url": "./static/js/2.ca421a37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ca421a37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "338c51124395d60b63ec",
+    "url": "./static/js/main.73fbca6a.chunk.js"
+  },
+  {
+    "revision": "9b26bcdd26997fbdd91f",
+    "url": "./static/js/runtime-main.b64816ec.js"
+  }
+]);
